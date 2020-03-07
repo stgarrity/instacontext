2cbab6e4d06993f66e30d30f7aa08872ed5f9c11
Unhack AI <cos+a@unhack.ai>
2020-03-07 18:20:40 +0000

    Function expression ⇒ `=>` function Converts a function expression to the new arrow function syntax.
    Arrow functions are more concise. 
    Also, they are and easier to reason about because `this` is resolved in the lexical scope, like any other variable.

--- background.js
+++ background.js
@@ -1,30 +1,30 @@
-var contextHandler = function (data) {
-    var xhr = new XMLHttpRequest();
-    // FIXME creds
+var contextHandler = data => {
+  var xhr = new XMLHttpRequest();
+  // FIXME creds
     var username = localStorage["username"];
-    var password = localStorage["password"];
+  var password = localStorage["password"];
 
-    if (!username) {
-	alert("Please configure your username in Extensions -> InstaContext -> Options");
-	return;
+  if (!username) {
+    alert("Please configure your username in Extensions -> InstaContext -> Options");
+    return;
+  }
+
+  var url = "https://www.instapaper.com/api/add?username=";
+  url += username + "&password=" + password + "&url=";
+
+  if (data.linkUrl) {
+    url += encodeURIComponent(data.linkUrl);
+  } else {
+    url += encodeURIComponent(data.pageUrl);
+  }
+
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4 && xhr.status == 201) {
+      alert("Saved");
     }
-
-    var url = "https://www.instapaper.com/api/add?username=";
-    url += username + "&password=" + password + "&url=";
-
-    if (data.linkUrl) {
-	url += encodeURIComponent(data.linkUrl);
-    } else {
-	url += encodeURIComponent(data.pageUrl);
-    }
-
-    xhr.onreadystatechange = function () {
-	if (xhr.readyState == 4 && xhr.status == 201) {
-	    alert("Saved");
-	}
-    };
-    xhr.open("GET", url);
-    xhr.send();
+  };
+  xhr.open("GET", url);
+  xhr.send();
 };
 
 chrome.contextMenus.create({
